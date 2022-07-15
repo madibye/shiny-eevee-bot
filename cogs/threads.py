9ce7ac876debe0bb2063e1f5659bd8edfc6c6a44
Madi Simpson <madibyee@gmail.com>
2022-07-15 11:04:13 -0700

    threads: port over !tka

---
+++ cogs/threads.py
@@ -0,0 +1,33 @@
+from discord import Thread
+from discord.ext import commands
+from discord.ext.commands import Context
+
+from helpers import db
+
+class Threads(commands.Cog, name="Threads"):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @commands.command(name="threadkeepalive", aliases=["tka"])
+    @commands.has_any_role("Discord Manager", "Lead Assistant")
+    async def thread_keep_alive(self, ctx: Context):
+        if not isinstance(ctx.channel, Thread):
+            return await ctx.send("To keep a thread alive, use this command in a thread!")
+        keep_alive_threads = db.get_keep_alive_threads()
+        if ctx.channel.id not in keep_alive_threads:
+            db.add_keep_alive_thread(ctx.channel.id)
+            return await ctx.send("Okie, I'll keep this thread alive for you! Use `!threadkeepalive` again in this "
+                                  "channel if you want me to stop reviving this thread!")
+        db.remove_keep_alive_thread(ctx.channel.id)
+        return await ctx.send("Alright, I won't keep this thread alive anymore!")
+
+    @commands.Cog.listener()
+    async def on_thread_update(self, before: Thread, after: Thread):
+        if not before.archived and after.archived:  # when a thread archival happens
+            keep_alive_threads = db.get_keep_alive_threads()
+            if after.id in keep_alive_threads:
+                return await after.edit(archived=False)
+
+
+async def setup(client):
+    await client.add_cog(Threads(client))
--- helpers/db.py
+++ helpers/db.py
@@ -4,6 +4,7 @@ import config
 mongo_client = MongoClient(config.mongo_url)
 database = mongo_client.staff_bot  # Database
 db_reminders = database.reminders  # Mongo Collection
+db_threads = database.threads
 
 
 def add_reminder(data):
@@ -17,3 +18,29 @@ def delete_reminder(document):
 
 def get_all_reminders():
     return db_reminders.find({})
+
+def create_thread_keep_alive_list():
+    thread_list_doc = {
+        "_id": "keep_alive_threads",
+        "threads": []  # List[int]
+    }
+    db_threads.insert_one(thread_list_doc)
+    return thread_list_doc
+
+def add_keep_alive_thread(thread_id):
+    thread_list_doc = db_threads.find_one({"_id": "keep_alive_threads"})
+    if thread_id not in thread_list_doc["threads"]:
+        thread_list_doc["threads"].append(thread_id)
+        return db_threads.replace_one({"_id": "keep_alive_threads"}, thread_list_doc)
+
+def remove_keep_alive_thread(thread_id):
+    thread_list_doc = db_threads.find_one({"_id": "keep_alive_threads"})
+    if thread_id in thread_list_doc["threads"]:
+        thread_list_doc["threads"].remove(thread_id)
+        return db_threads.replace_one({"_id": "keep_alive_threads"}, thread_list_doc)
+
+def get_keep_alive_threads():
+    thread_list_doc = db_threads.find_one({"_id": "keep_alive_threads"})
+    if not thread_list_doc:
+        thread_list_doc = create_thread_keep_alive_list()
+    return thread_list_doc["threads"]
